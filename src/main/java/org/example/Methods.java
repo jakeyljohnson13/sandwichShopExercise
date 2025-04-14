004e046b586fa035161cd82759d14dc79564004f
jakeyljohnson13 <jakeyl.johnson13@yahoo.com>
2025-04-14 15:32:11 -0700

    Caught mistakes on sandwich shop
    Loaded sandwich upgrade changed to $1.75, discount print statements changed to reflect correct discount type, Added text to display what kind of sandwich is being purchased.

--- src/main/java/org/example/Methods.java
+++ src/main/java/org/example/Methods.java
@@ -7,7 +7,7 @@ public class Methods {
     static String isLoaded;
 
     public static double basePrice(){
-        System.out.println("Would you like you to upgrade to a loaded sandwich (Y/N) for $1?");
+        System.out.println("Would you like you to upgrade to a loaded regular sandwich (Y/N) for $1?");
         isLoaded = m.nextLine();
         if (isLoaded.equals("N")){
             total = 5.45;
@@ -21,14 +21,14 @@ public class Methods {
         return Math.round(total * 100.0) / 100.0;
     }
     public static double youngBase(){
-        System.out.println("Would you like you to upgrade to a loaded sandwich (Y/N) for $1?");
+        System.out.println("Would you like you to upgrade to a loaded regular sandwich (Y/N) for $1?");
         isLoaded = m.nextLine();
         if (isLoaded.equals("N")) {
             System.out.println("You got the Student discount on your regular sandwich.");
             total = 5.45 - (5.45 * 0.1);
         }
         else if (isLoaded.equals("Y")) {
-            System.out.println("You got the Student discount on your regular sandwich.");
+            System.out.println("You got the Student discount on your loaded regular sandwich.");
             total = 6.45 - (6.45 * 0.1);
         }
         else {
@@ -38,14 +38,14 @@ public class Methods {
         return Math.round(total * 100.0) / 100.0;
     }
     public static double oldBase(){
-        System.out.println("Would you like you to upgrade to a loaded sandwich (Y/N) for $1?");
+        System.out.println("Would you like you to upgrade to a loaded regular sandwich (Y/N) for $1?");
         isLoaded = m.nextLine();
         if (isLoaded.equals("N")) {
-            System.out.println("You got the Student discount on your regular sandwich.");
+            System.out.println("You got the Senior discount on your regular sandwich.");
             total = 5.45 - (5.45 * 0.2);
         }
         else if (isLoaded.equals("Y")) {
-            System.out.println("You got the Student discount on your regular sandwich.");
+            System.out.println("You got the Senior discount on your loaded regular sandwich.");
             total = 6.45 - (6.45 * 0.2);
         }
         else {
@@ -55,13 +55,13 @@ public class Methods {
         return Math.round(total * 100.0) / 100.0;
     }
     public static double largeBase(){
-        System.out.println("Would you like you to upgrade to a loaded sandwich (Y/N) for $1?");
+        System.out.println("Would you like you to upgrade to a loaded large sandwich (Y/N) for $1.75?");
         isLoaded = m.nextLine();
         if (isLoaded.equals("N")){
             total = 8.95;
         }
         else if (isLoaded.equals("Y")) {
-            total = 9.95;
+            total = 10.7;
         }
         else{
             System.out.println("Invalid Input.");
@@ -70,15 +70,15 @@ public class Methods {
         return Math.round(total * 100.0) / 100.0;
     }
     public static double youngLarge(){
-        System.out.println("Would you like you to upgrade to a loaded sandwich (Y/N) for $1?");
+        System.out.println("Would you like you to upgrade to a loaded large sandwich (Y/N) for $1.75?");
         isLoaded = m.nextLine();
         if (isLoaded.equals("N")) {
-            System.out.println("You got the Student discount on your regular sandwich.");
+            System.out.println("You got the Student discount on your large sandwich.");
             total = 8.95 - (8.95 * 0.1);
         }
         else if (isLoaded.equals("Y")) {
-            System.out.println("You got the Student discount on your regular sandwich.");
-            total = 9.95 - (9.95 * 0.1);
+            System.out.println("You got the Student discount on your loaded large sandwich.");
+            total = 10.7 - (10.7 * 0.1);
         }
         else {
             System.out.println("Invalid Input");
@@ -87,15 +87,15 @@ public class Methods {
         return Math.round(total * 100.0) / 100.0;
     }
     public static double oldLarge(){
-        System.out.println("Would you like you to upgrade to a loaded sandwich (Y/N) for $1?");
+        System.out.println("Would you like you to upgrade to a loaded large sandwich (Y/N) for $1.75?");
         isLoaded = m.nextLine();
         if (isLoaded.equals("N")) {
-            System.out.println("You got the Senior discount on your regular sandwich.");
+            System.out.println("You got the Senior discount on your large sandwich.");
             total = 8.95 - (8.95 * 0.2);
         }
         else if (isLoaded.equals("Y")) {
-            System.out.println("You got the Student discount on your regular sandwich.");
-            total = 9.95 - (9.95 * 0.2);
+            System.out.println("You got the Senior discount on your loaded large sandwich.");
+            total = 10.7 - (10.7 * 0.2);
         }
         else {
             System.out.println("Invalid Input");
